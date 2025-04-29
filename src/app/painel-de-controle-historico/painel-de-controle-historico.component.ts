f8e18f1fcb1f423997189c542e3988513e4e33c6
Pedro <pedro.lima@sptech.school>
2025-04-29 19:23:10 -0300

    Update:  HTML - Painel de controle

---
+++ src/app/painel-de-controle-historico/painel-de-controle-historico.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-painel-de-controle-historico',
+  imports: [],
+  templateUrl: './painel-de-controle-historico.component.html',
+  styleUrl: './painel-de-controle-historico.component.css'
+})
+export class PainelDeControleHistoricoComponent {
+
+}
