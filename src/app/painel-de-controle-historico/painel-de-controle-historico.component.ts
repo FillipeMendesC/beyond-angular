91edbb16bf8f9f6adef6618dcc11ce4a2cb7e1e3
Pedro <pedro.lima@sptech.school>
2025-04-30 14:35:13 -0300

    UPDATE - finalizações painel de controle historico

--- src/app/painel-de-controle-historico/painel-de-controle-historico.component.ts
+++ src/app/painel-de-controle-historico/painel-de-controle-historico.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { CardKpiComponent } from '../components/card-kpi/card-kpi.component';
 
 @Component({
   selector: 'app-painel-de-controle-historico',
-  imports: [],
+  imports: [CommonModule, CardKpiComponent],
   templateUrl: './painel-de-controle-historico.component.html',
   styleUrl: './painel-de-controle-historico.component.css'
 })
 export class PainelDeControleHistoricoComponent {
-
+    @Input() titulo:string = '';
 }
+
+
